ee26e9eac537c316b52fed385b3b3ee5c0796e29
0788231715 <abeliniyigena@gmail.com>
2026-05-21 15:22:28 +0200

    Initial commit - Sales Web platform

--- Bika_mushroom/sales/homevista-backend/audit/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import AuditEvent
-
-class AuditEventSerializer(serializers.ModelSerializer):
-    user_email = serializers.EmailField(source='user.email', read_only=True)
-
-    class Meta:
-        model = AuditEvent
-        fields = '__all__'
